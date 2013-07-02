12845dc63fdaa86d31ecfe71c55b9adf34db9d32
huangyingw@linux_programming <huangyingw@gmail.com>
2013-07-02 14:19:09 -0700

    minor fix on xtest.sh

--- xtest.sh
+++ xtest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo /opt/systems/bin/xetus-tomcat stop \
   && sudo xetus/rebuild.bash -sv \
-  && sudo /opt/systems/bin/xetus-tomcat start
+  && sudo /opt/systems/bin/xetus-tomcat start \
   && ant -f /opt/xetusbase/xetus/test/build.xml reload-db && ant -f /opt/xetusbase/xetus/test/build.xml
