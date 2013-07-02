359bca95be16093dc151dfb3e0e9111272c2cf58
huangyingw@linux_programming <huangyingw@gmail.com>
2013-07-02 14:15:57 -0700

    update on xtest.sh file

--- xtest.sh
+++ xtest.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-ant -f /opt/xetusbase/xetus/test/build.xml reload-db && ant -f /opt/xetusbase/xetus/test/build.xml
+sudo /opt/systems/bin/xetus-tomcat stop \
+  && sudo xetus/rebuild.bash -sv \
+  && sudo /opt/systems/bin/xetus-tomcat start
+  && ant -f /opt/xetusbase/xetus/test/build.xml reload-db && ant -f /opt/xetusbase/xetus/test/build.xml
