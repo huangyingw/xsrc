b3c983ce7d5f27e0708ae462ee7e8f622a60edfa
huangyingw@linux_programming <huangyingw@gmail.com>
2013-07-02 16:21:06 -0700

    update on xtest.sh

--- xtest.sh
+++ xtest.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+if [ -z "$1" ];
+then
+  echo "Please provide commit message"
+  exit 1
+fi
 sudo /opt/systems/bin/xetus-tomcat stop \
   && sudo xetus/rebuild.bash -sv \
   && sudo /opt/systems/bin/xetus-tomcat start \
-  && ant -f /opt/xetusbase/xetus/test/build.xml reload-db && ant -f /opt/xetusbase/xetus/test/build.xml
+  && ant -f /opt/xetusbase/xetus/test/build.xml reload-db && ant -f /opt/xetusbase/xetus/test/build.xml \
+  && ~/gitrc/g.sh "$1"
