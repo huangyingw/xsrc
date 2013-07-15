36e2273be1df37c86e6fb70a01dd16958606ae9e
huangyingw@linux_programming <huangyingw@gmail.com>
2013-07-15 12:46:54 -0700

    xtest.sh works well

--- xtest.sh
+++ xtest.sh
@@ -4,8 +4,9 @@ then
   echo "Please provide commit message"
   exit 1
 fi
-sudo /opt/systems/bin/xetus-tomcat stop \
-  && sudo xetus/rebuild.bash -sv \
+sudo chmod -R 777 tmp/ logs/ \
+  && sudo /opt/systems/bin/xetus-tomcat stop \
+  ; sudo xetus/rebuild.bash -sv \
   && sudo /opt/systems/bin/xetus-tomcat start \
   && ant -f /opt/xetusbase/xetus/test/build.xml reload-db && ant -f /opt/xetusbase/xetus/test/build.xml \
   && ~/gitrc/g.sh "$1"
