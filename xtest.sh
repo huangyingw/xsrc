1010a28981ee64ef13fcc3f8190fe4762c75528e
huangyingw@linux_programming <huangyingw@gmail.com>
2013-07-17 09:04:54 -0700

    update xtest.sh

--- xtest.sh
+++ xtest.sh
@@ -8,5 +8,7 @@ sudo chmod -R 777 tmp/ logs/ \
   && sudo /opt/systems/bin/xetus-tomcat stop \
   ; sudo xetus/rebuild.bash -sv \
   && sudo /opt/systems/bin/xetus-tomcat start \
-  && ant -f /opt/xetusbase/xetus/test/build.xml reload-db && ant -f /opt/xetusbase/xetus/test/build.xml \
+  && ant -f /opt/xetusbase/xetus/test/build.xml reload-db \
+  && sudo chmod -R 777 tmp/ logs/ \
+  && ant -f /opt/xetusbase/xetus/test/build.xml \
   && ~/gitrc/g.sh "$1"
