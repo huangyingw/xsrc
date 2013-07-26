e66951421d1c6e1478ceaddf7b4ee5034afc1151
huangyingw@linux_programming <huangyingw@gmail.com>
2013-07-26 15:34:15 -0700

    update on xinit.sh

--- xinit.sh
+++ xinit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo /opt/systems/bin/xetus-tomcat stop \
   && cp -fv /opt/xetusbase/.gitignore ./ \
+  && ln -s /opt/sql sql \
   && sudo xetus/rebuild.bash -i \
   && sudo /opt/systems/bin/xetus-tomcat start \
   && git init && git add . && git ci -am "n" && git gc
