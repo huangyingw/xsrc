dac27abd91a376c4ffb12e283dab68ac6d57522d
huangyingw@linux_programming <huangyingw@gmail.com>
2013-08-07 14:44:18 -0700

    update on rebuild.sh, xtest.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo /opt/systems/bin/xetus-tomcat stop \
   && sudo xetus/rebuild.bash -sv \
-  && sudo /opt/systems/bin/xetus-tomcat start
+  && sudo /opt/systems/bin/xetus-tomcat start \
+  && echo now begin to tail log to find.cc, when you finish, press ctrl-c \
+  && tail -f logs/tomcat/xetus-tomcat.log > find.cc
--- xtest.sh
+++ xtest.sh
@@ -4,11 +4,11 @@ then
   echo "Please provide commit message"
   exit 1
 fi
-  sudo /opt/systems/bin/xetus-tomcat stop \
+sudo /opt/systems/bin/xetus-tomcat stop \
   ; sudo xetus/rebuild.bash -sv \
   && sudo /opt/systems/bin/xetus-tomcat start \
   && ant -f /opt/xetusbase/xetus/test/build.xml reload-db \
   && sudo chmod -R 777 tmp/ logs/ \
   && ant -f /opt/xetusbase/xetus/test/build.xml \
-  && ~/gitrc/g.sh "$1" \
-  && ~/p4rc/p4u.sh
+  && git commit  --no-verify -am "$1" && git gc \
+  && ~/gitrc/gps.sh
