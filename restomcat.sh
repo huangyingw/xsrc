48be8355b6a63a0d05b3a0e415325d48bac1fb7a
huangyingw@linux_programming <huangyingw@gmail.com>
2013-09-20 11:50:05 -0700

    kill the tomcat instead of using stop command

--- restomcat.sh
+++ restomcat.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sudo /opt/systems/bin/xetus-tomcat restart
+pid=`ps ax|awk '/xetusbase\/tomcat/{print $1}'`
+if [ -n "$pid" ];
+then
+    sudo kill -9 $pid
+fi
+sudo /opt/systems/bin/xetus-tomcat start
