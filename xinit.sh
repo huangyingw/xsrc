63fc66b2c843dc9eb653240ddefe260dda066360
huangyingw@linux_programming <huangyingw@gmail.com>
2013-11-06 10:18:56 -0800

    n

--- xinit.sh
+++ xinit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-running_home=`ps ax|grep -o "\ /[U]sers.*/tomcat"|sed -e "s|tomcat||"`
-if [ -d "$running_home" ];
+running_home=`ps ax|grep -o "\ /[U]sers.*/tomcat"|sed -e "s|\/tomcat||"`
+if [ -d $running_home ];
 then
     "$running_home"tomcat/bin/xetusone.bash stop \
         ; cp -fv "$running_home".gitignore ./ \
