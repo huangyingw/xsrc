70362f2da98d3253a76fe02f6e1a312dd9dc1e37
huangyingw@linux_programming <huangyingw@gmail.com>
2013-11-12 12:50:04 -0800

    n

--- restomcat.sh
+++ restomcat.sh
@@ -4,7 +4,6 @@ echo "$running_home"
 if [ -d $running_home ];
 then
     echo restart previous
-    eval "$running_home"tomcat/bin/xetusone.bash restart
     eval "$running_home"tomcat/bin/xetusone.bash stop
 fi
 tomcat/bin/xetusone.bash start
