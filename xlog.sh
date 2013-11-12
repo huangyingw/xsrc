0a505e18e042853f6c84e9c58fd5043c58e8168e
huangyingw@linux_programming <huangyingw@gmail.com>
2013-11-12 13:34:14 -0800

    n

--- xlog.sh
+++ xlog.sh
@@ -5,8 +5,8 @@ then
     TARGET="$1"
 fi
 echo now, begin to log
-running_home=`ps ax|grep -o "\ /Users.*/tomcat"|awk 'NR==1{print $1}'|sed -e "s|tomcat||"`
-if [ -d "$running_home" ];
+running_home=`ps ax|grep -o "\ /[U]sers.*/tomcat"|sed -e "s|tomcat||"`
+if [ -d $running_home ];
 then
-    tail -f "$running_home"logs/xetusone.log > "${TARGET}"
+    eval tail -f "$running_home"logs/xetusone.log > "${TARGET}"
 fi
